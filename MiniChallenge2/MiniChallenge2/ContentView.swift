8b5dbf1e84d15f3c4c20820a8cdb485ba7280fca
Lucca-Lopes <102756745+Lucca-Lopes@users.noreply.github.com>
2022-11-16 15:29:33 -0300

    Projeto pronto em swiftUI

---
+++ MiniChallenge2/MiniChallenge2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniChallenge2
+//
+//  Created by Lucca Lopes on 16/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniChallenge2/MiniChallenge2/MiniChallenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge2App.swift
+//  MiniChallenge2
+//
+//  Created by Lucca Lopes on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
