5088d18094cb8cb1e02bc23984b12530fbb7616b
Caroline Stelitano <56271183+Caroline-Stelitano@users.noreply.github.com>
2022-11-22 13:27:02 -0300

    fç createButton

--- MiniChallenge2/MiniChallenge2/ConfiguracoesView.swift
+++ MiniChallenge2/MiniChallenge2/ConfiguracoesView.swift
@@ -9,72 +9,90 @@ import Foundation
 import SwiftUI
 
 struct ConfiguracoesView: View {
-    var musica = true
-    var efeitosSonoros = true
+    @State var musica = true
+    @State var efeitosSonoros = true
     @State var showModal1 = false
     @State var showModal2 = false
     @State var showModal3 = false
     
     var body: some View{
         VStack{
-            // inserir Música
-            // inserir Efeitos Sonoros
+            Spacer()
+                .frame(height: 20)
             
-            //Aba: Sobre
+            // inserir Música
             HStack{
                 Button(action: {
-                    showModal1 = true
-                }, label: {
-                    Text("Sobre")
+                    musica = false
+                },label: {
+                    Image(systemName: "speaker.square")
+                    Text("Música")
                         .font(.system(size: 17, design: .rounded))
+                        .foregroundColor(.black)
                         .bold()
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.leading, 10)
+                        .padding(.bottom, 10)
                 })
-                .foregroundColor(.white)
-                .background(Color(red: 0.851, green: 0.851, blue: 0.851))
-                .buttonStyle(.bordered)
-                .cornerRadius(10)
-                .buttonBorderShape(.automatic)
-                .controlSize(.large)
             }
+            
+            // inserir Efeitos Sonoros
+            HStack{
+                Button(action: {
+                    efeitosSonoros = false
+                },label: {
+                    Image(systemName: "speaker.circle")
+                    Text("Efeitos Sonoros")
+                        .font(.system(size: 17, design: .rounded))
+                        .foregroundColor(.black)
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 10)
+                        .padding(.bottom, 10)
+                })
+            }
+            
+            Spacer()
+                .frame(height: 30)
+            
+            //Aba: Sobre
+            createButton(action2: {
+                showModal1 = true
+            }, label2: "Sobre")
 //preciso escutar a mudança desse State para conseguir mostrar uma tela na frente
-//$showModal1 é um binding
+//$showModal1 é um binding do tipo booleano
             .sheet(isPresented: $showModal1){
                 SobreView()
             }
-        }
-        
-        //Aba: Política de Privacidade
-        HStack{
-            Button(action: {
+            
+            //Aba: Política de Privacidade
+            createButton(action2: {
                 showModal2 = true
-                
-            }, label: {
-                Text("Política de Privacidade")
-                    .font(.system(size: 17, design: .rounded))
-                    .bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 10)
-            })
-            .foregroundColor(.white)
-            .background(Color(red: 0.851, green: 0.851, blue: 0.851))
-            .buttonStyle(.bordered)
-            .cornerRadius(10)
-            .buttonBorderShape(.automatic)
-            .controlSize(.large)
-        }
-        .sheet(isPresented: $showModal2){
-            PoliticaPrivacidadeView()
-        }
-        
-        //Aba: Termos de Uso
-        HStack{
-            Button(action: {
+            }, label2: "Política de Privacidade")
+            .sheet(isPresented: $showModal2){
+                PoliticaPrivacidadeView()
+            }
+            
+            //Aba: Termos de Uso
+            createButton(action2: {
                 showModal3 = true
-                
-            }, label: {
-                Text("Termos de Uso")
+            }, label2: "Termos de Uso")
+            .sheet(isPresented: $showModal3){
+                TermosUsoView()
+            }
+            
+            Spacer()
+                .navigationTitle("Info")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+        .padding(20)
+    }
+    
+    func createButton(action2: @escaping () -> Void, label2: String) -> some View {
+        return
+            Button(action: action2,
+                   label: {
+                Text(label2)
                     .font(.system(size: 17, design: .rounded))
                     .bold()
                     .frame(maxWidth: .infinity, alignment: .leading)
@@ -86,13 +104,5 @@ struct ConfiguracoesView: View {
             .cornerRadius(10)
             .buttonBorderShape(.automatic)
             .controlSize(.large)
-        }
-        .sheet(isPresented: $showModal3){
-            TermosUsoView()
-        }
-        Spacer()
-            .navigationTitle("Info")
-            .navigationBarTitleDisplayMode(.inline)
-            .padding()
     }
 }
