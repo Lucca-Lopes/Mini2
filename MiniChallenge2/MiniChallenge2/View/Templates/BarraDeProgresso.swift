8f88fb1a4aab4684f222f4596566cef84027bd7b
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2022-11-30 14:34:54 -0300

    barra de progresso crescente

--- MiniChallenge2/MiniChallenge2/View/Templates/BarraDeProgresso.swift
+++ MiniChallenge2/MiniChallenge2/View/Templates/BarraDeProgresso.swift
@@ -16,7 +16,7 @@ class BarraDeProgresso: SKNode{
     override init() {
         self.borda = SKSpriteNode(color: .black, size: .init(width: 30, height: 10))
         self.bordaInterna = SKSpriteNode(color: .gray, size: .init(width: 27, height: 7))
-        self.barra = SKSpriteNode(color: .green, size: .init(width: 27, height: 7))
+        self.barra = SKSpriteNode(color: .green, size: .init(width: 0, height: 7))
         super.init()
         setup()
     }
@@ -26,18 +26,18 @@ class BarraDeProgresso: SKNode{
     }
     
     public func timer(tempo: CGFloat){
-        let barraPorcentagem = self.barra.size.width / tempo
+        let barraPorcentagem = self.bordaInterna.size.width / tempo
         
         let diminuiContagem = SKAction.repeat(.sequence([
             .run {
-                self.barra.size.width -= barraPorcentagem
+                self.barra.size.width += barraPorcentagem
             },
             .wait(forDuration: 1)
         ]), count: Int(tempo))
         
         barra.run(.sequence([
             .wait(forDuration: 1),
-            diminuiContagem
+            diminuiContagem,
         ]))
     }
     
@@ -47,6 +47,7 @@ class BarraDeProgresso: SKNode{
         addChild(barra)
         barra.zPosition += 5
         barra.anchorPoint = .init(x: 0, y: 0.5)
-        barra.position.x = -(barra.size.width/2)
+//        barra.position.x = -(barra.size.width/2)
+        barra.position.x = bordaInterna.position.x - bordaInterna.size.width/2
     }
 }
