e8d7cff78a5e4071d34bc79e83242f738cd5367f
Lucca-Lopes <luccavlopes@hotmail.com>
2022-12-01 15:29:09 -0300

    pre config precos

--- MiniChallenge2/MiniChallenge2/View/GameScene.swift
+++ MiniChallenge2/MiniChallenge2/View/GameScene.swift
@@ -29,13 +29,13 @@ class GameScene: SKScene{
         
         
         //Em posMausoleu, para criar a área na direita, passa-se 1 como parâmetro do x. Para criar na esquerda, passa-se 3.
-        let mausoleu1 = MausoleuView(posMausoleu: CGPoint(x: 1, y: 0.7), nome: "pobre", ativo: vm.mausoleus[0], cova1Ativa: vm.covas[0], cova2Ativa: vm.covas[1], dinheiroGanho: 5, tempoCova: 5)
-        let mausoleu2 = MausoleuView(posMausoleu: CGPoint(x: 1, y: 0.45), nome: "artista", ativo: vm.mausoleus[1], cova1Ativa: vm.covas[2], cova2Ativa: vm.covas[3], dinheiroGanho: 30, tempoCova: 15)
-        let mausoleu3 = MausoleuView(posMausoleu: CGPoint(x: 1, y: 0.2), nome: "militar", ativo: vm.mausoleus[2], cova1Ativa: vm.covas[4], cova2Ativa: vm.covas[5], dinheiroGanho: 60, tempoCova: 25)
+        let mausoleu1 = MausoleuView(posMausoleu: CGPoint(x: 1, y: 0.7), nome: "pobre", ativo: vm.mausoleus[0], cova1Ativa: vm.covas[0], cova2Ativa: vm.covas[1], dinheiroGanho: 5, tempoCova: 5, precoMausoleu: 0, precoCova: 50)
+        let mausoleu2 = MausoleuView(posMausoleu: CGPoint(x: 1, y: 0.45), nome: "artista", ativo: vm.mausoleus[1], cova1Ativa: vm.covas[2], cova2Ativa: vm.covas[3], dinheiroGanho: 30, tempoCova: 15, precoMausoleu: 100, precoCova: 50)
+        let mausoleu3 = MausoleuView(posMausoleu: CGPoint(x: 1, y: 0.2), nome: "militar", ativo: vm.mausoleus[2], cova1Ativa: vm.covas[4], cova2Ativa: vm.covas[5], dinheiroGanho: 60, tempoCova: 25, precoMausoleu: 100, precoCova: 50)
         
-        let mausoleu4 = MausoleuView(posMausoleu: CGPoint(x: 3, y: 0.7), nome: "trabalhador", ativo: vm.mausoleus[3], cova1Ativa: vm.covas[6], cova2Ativa: vm.covas[7], dinheiroGanho: 15, tempoCova: 10)
-        let mausoleu5 = MausoleuView(posMausoleu: CGPoint(x: 3, y: 0.45), nome: "empresario", ativo: vm.mausoleus[4], cova1Ativa: vm.covas[8], cova2Ativa: vm.covas[9], dinheiroGanho: 45, tempoCova: 20)
-        let mausoleu6 = MausoleuView(posMausoleu: CGPoint(x: 3, y: 0.2), nome: "rei", ativo: vm.mausoleus[5], cova1Ativa: vm.covas[10], cova2Ativa: vm.covas[11], dinheiroGanho: 90, tempoCova: 30)
+        let mausoleu4 = MausoleuView(posMausoleu: CGPoint(x: 3, y: 0.7), nome: "trabalhador", ativo: vm.mausoleus[3], cova1Ativa: vm.covas[6], cova2Ativa: vm.covas[7], dinheiroGanho: 15, tempoCova: 10, precoMausoleu: 100, precoCova: 50)
+        let mausoleu5 = MausoleuView(posMausoleu: CGPoint(x: 3, y: 0.45), nome: "empresario", ativo: vm.mausoleus[4], cova1Ativa: vm.covas[8], cova2Ativa: vm.covas[9], dinheiroGanho: 45, tempoCova: 20, precoMausoleu: 100, precoCova: 50)
+        let mausoleu6 = MausoleuView(posMausoleu: CGPoint(x: 3, y: 0.2), nome: "rei", ativo: vm.mausoleus[5], cova1Ativa: vm.covas[10], cova2Ativa: vm.covas[11], dinheiroGanho: 90, tempoCova: 30, precoMausoleu: 100, precoCova: 50)
         
         addChild(spriteMoeda)
         
